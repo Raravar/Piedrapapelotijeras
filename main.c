645f65222a1eb34f03f9965ccc3ef11d4a81d3e7
Raravar <ricardo.daravena@hotmail.com>
2022-08-09 01:41:52 -0400

    Creando variable.txt

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char jugador[10] = "data";
+char jugador[10] = "";
 unsigned char puntuacion = 0;
 
 void registro()
@@ -16,8 +16,9 @@ void registro()
     printf("Ingrese su nombre de jugador : ");
     scanf("%s", jugador);
     printf("Bienvenido %s", jugador);
-    FILE *fp;
-    fp  = fopen ("data.txt", "w");
+    //FILE *fp;
+    strcat(jugador, ".txt");
+    FILE * archivo = fopen(jugador, "w+");
 };
 
 int main()
