a2970f0f4d7ba1d4330b69799753fa967b260d82
Raravar <ricardo.daravena@hotmail.com>
2022-08-09 01:51:45 -0400

    Guardado de puntuacion en variable.txt

--- main.c
+++ main.c
@@ -9,16 +9,16 @@
 #include <stdlib.h>
 
 char jugador[10] = "";
-unsigned char puntuacion = 0;
+unsigned char puntuacion = 5;
 
 void registro()
 {
     printf("Ingrese su nombre de jugador : ");
     scanf("%s", jugador);
     printf("Bienvenido %s", jugador);
-    //FILE *fp;
     strcat(jugador, ".txt");
     FILE * archivo = fopen(jugador, "w+");
+    fprintf(archivo,"%d", puntuacion);
 };
 
 int main()
